b0eeac77f06c2666bd7dfaaf7fb49a34ae3f6006
Matt Edelman <mattedelman@gmail.com>
2012-08-10 14:30:50 -0700

    fixing some syntax issues

--- sandwi.js
+++ sandwi.js
@@ -1,11 +1,21 @@
-(function (){
-	window.Sandwi = {js:null};
-	var defaults = {bread: "rye",meat: "roast beef",condiments: ["mayo","mustard","lettuce","tomato","pickles"]};
-	window.Sandwi.js = function(config) {
+/**
+* Comments are for the weak
+*
+*/
+(function() {
+	window.Sandwi = {
+		js: null
+	};
+	var defaults = {
+		bread: "rye",
+		meat: "roast beef",
+		condiments: ["mayo", "mustard", "lettuce", "tomato", "pickles"]
+	};
+	window.Sandwi.js = {
 		var sandwich = (config) ? config : defaults;
-		
-};
+		this.toString = function() {return "I am a sandwich"};
+	};
 	window.Sandwi.js.prototype.toString = function() {
 		return "I am a sandwich";
-}
+	}
 })();
