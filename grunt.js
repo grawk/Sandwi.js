1421a981227678e8723fcd665f266f28c9078abd
grawk <mattedelman@gmail.com>
2013-02-04 10:34:09 -0800

    adding grunt config

---
+++ grunt.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		lint: {
+			all: ['grunt.js', 'lib/**/*.js', 'public/js/lib/paypal/**/*.js', 
+			'public/js/controller/**/*.js', 'public/js/core/**/*.js', 'public/js/view/**/*.js']
+		},
+		jshint: {
+			options: {
+				browser: true
+			}
+		}
+	});
+
+	
+	// Default task.
+	grunt.registerTask('default', 'lint');
+
+	grunt.registerTask('test', 'run mocha-phantomjs', function() {
+		var done = this.async();
+		require('child_process').exec('mocha-phantomjs ./mocha-runner/test.html', function(err, stdout) {
+			grunt.log.write(stdout);
+			done(err);
+		});
+	});
+};
